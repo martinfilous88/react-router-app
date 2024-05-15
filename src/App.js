5a402e5631fbb7b036a9675caccf74418dc5aea9
Martin Filous <martin.filous@beanstudent.cz>
2024-05-15 09:33:38 +0200

    OKAAAY

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./Home";
-import NoPage from "./NoPage1";
-import Pocitadlo from "./Pocitadlo";
-import Kavy from "./Kavy";
-import Ukolnicek from "./ukolnicek/Ukolnicek";
-import Kalkulacka from "./kalkulacka/Kalkulacka";
-import Eshop from "./Eshop";
-import OblibenaBarva from "./barva/barva";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Paper from "@mui/material/Paper";
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
-export default function App() {
+
+function App() {
+
+  const [filmy, nastavFilmy] = useState([])
+  const [vybranyFilm, nastavVybranyFilm] = useState("")
+
+  
+
+
+
+  useEffect(() => {
+    axios.get('https://api.sampleapis.com/cartoons/cartoons2D')
+      .then(odpoved => {
+        nastavFilmy(odpoved.data)
+      }
+
+      )
+
+  }, [])
+
   return (
-    <HashRouter basename='/'>
-      <Layout/>
-      <Routes>
-          <Route index element = {<Home />} />
-          <Route path="/pocitadlo" element={<Pocitadlo />} />
-          <Route path="/kavy" element={<Kavy />} />
-          <Route path="/ukolnicek" element={<Ukolnicek />} />
-          <Route path="/kalkulacka" element={<Kalkulacka />} />
-          <Route path="/eshop" element={<Eshop />} />
-          <Route path="/barva " element={<OblibenaBarva />} />
-              <Route path="*" element={<NoPage />} />
+    <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Box sx={{ p: 3 }}>
+            <Typography variant="h3" >Výběr filmu</Typography> <br />
+            {vybranyFilm !== "" && (
+                <>
+                    <Typography variant="h6">Vybraný film je: {vybranyFilm}</Typography> <br />
+                    <br /><br />
+                </>
+            )}
+            <Grid container spacing={2}>
+                {filmy.map(filmy => (
+                    <Grid item xs={12} sm={5} md={4} lg={3} key={filmy.id} style={{ listStyle: "none" }}>
+                        <Paper elevation={4} onClick={() => {
+                            nastavVybranyFilm(filmy.title);
+                        }} style={{ textAlign: "center", cursor: "pointer", padding: 10 }}>
+                            <img src={filmy.image} alt={filmy.title} style={{ width: "100px" }} /> <br />
+                            <Typography variant="overline">{filmy.title}</Typography> <br />
+                            <Typography variant="overline">{filmy.year}</Typography>
+                        </Paper>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    </ThemeProvider>
+);
+}
 
-              
-            </Routes>
-          </HashRouter>
-          )
-};
+export default App;
--- src/Layout.js
+++ src/Layout.js
@@ -3,25 +3,19 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import MuiLink from '@mui/material/Link'
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
 
-export default function Layout() {
+export default function Layout(props) {
     return (
         <>
             <nav>
                 <ul>
-                    <Button href="/react-portfolio/#/" variant="h5">Home</Button>
-                    <Button href="/react-portfolio/#/pocitadlo" variant="h5">Počítadlo</Button>
-                    <Button href="/react-portfolio/#/kavy" variant="h5">Seznam káv (použití API)</Button>
-                    <Button href="/react-portfolio/#/ukolnicek" variant="h5">Úkolníček (použití MaterialUI)</Button>
-                    <Button href="/react-portfolio/#/kalkulacka" variant="h5">Kalkulačka</Button>
-                    <Button href="/react-portfolio/#/eshop" variant="h5">E-shop</Button>
-                    <Button href="/react-portfolio/#/barva" variant="h5">Barva</Button>
+                    <Button style={{color: props.barva}} href="/react-router-app/#" variant="h5">Home</Button>
+                    <Button style={{color: props.barva}} href="/react-router-app/#/kavy" variant="h5">Seznam animovaných filmů<span>&#128196;</span></Button>
                 </ul>
 
             </nav>
-            <Outlet />
+            
         </>
     )
 };
